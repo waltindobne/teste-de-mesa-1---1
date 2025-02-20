30a42ca904e16a0875fa013bf0dcef9ce80ab125
walterbrito <walter.222brito@gmail.com>
2025-02-20 13:57:44 -0300

    update

--- teste de mesa 1 - 1/Program.cs
+++ teste de mesa 1 - 1/Program.cs
@@ -9,7 +9,9 @@ namespace testeDemesa1
         static void Main(string[] args)
         {
             Objects objects = new Objects();
-            objects.Loop();
+            objects.Teste1();
+            objects.Teste2();
+            objects.Teste3();
         }
     }
     public class Objects
@@ -21,8 +23,9 @@ namespace testeDemesa1
         double[] v = new double [4];
         string vI; // representa o valor de i, no caso, se i = 5, retorna verdadeiro e se não retorna falso
 
-        public void Loop()
+        public void Teste1()
         {
+            Console.WriteLine($"Planilha 1 teste 1:");
             c1 = (a + b) / 2;
             c2 = c1 - 40;
             for (int i = 0; i < 4; i++)
@@ -37,6 +40,37 @@ namespace testeDemesa1
                 }
                 vI = (v[i] == 5) ? "Verdadeiro" : "Falso";
                 Console.WriteLine($"linha {i+1}, valor de a: {a}, valor de b: {b}, variavel c1: {c1}, variavel c2: {c2}, vetor v[{i}]: {v[i]}, condição do vetor: {vI} ");
+            }
+        }
+
+        public void Teste2()
+        {
+            Console.WriteLine($"Planilha 1 teste 2:");
+            int a = 2;
+            string c = "verdadeiro";
+            int i = 0;
+            while (a <= 6)
+            {
+                ++a;
+                i++;
+                if (a - 1 == 6)
+                {
+                    c = "Falso";
+                }
+                Console.WriteLine($"posição: {i - 1}, variavel: {a - 1}, vetor[{i - 1}]: {a * 10}, contador: {a}, condição: {c}");
+            }
+        }
+        public void Teste3()
+        {
+            Console.WriteLine($"Planilha 1 teste 3:");
+            int[] v = new int[10];
+            int a = 7;
+            int b = a - 7;
+            while (b < a)
+            {
+                v[b] = b + a;
+                b = b + 2;
+                Console.WriteLine($"valor de a: {a}, valor de b: {b}");
             }
         }
     }
